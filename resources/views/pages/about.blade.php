9d007cd0c4325fc66946d638e45f7e5e1f0dd44f
anchal <akr43@njit.edu>
2018-10-23 19:09:18 -0400

    Procfile for Heroku

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+
+
+
+        <div class="jumbotron">
+
+
+            <h1 >About Page</h1>
+            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+
+
+        </div>
+
+
+
+
+@endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anchal
+ * Date: 10/23/18
+ * Time: 9:10 AM
+ */
